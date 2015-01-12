f0c728b8b74b902911b153b4b5c8d50e1269ba40
Kuo Husan Hsu <khhsu1333@gmail.com>
2015-01-12 22:42:26 +0800

    Fix hamming distance calculate.

--- utils/utils.js
+++ utils/utils.js
@@ -2,11 +2,11 @@ module.exports = {
 	hamdist : function (s1, s2)
 	{  
 	   if(s1 == undefined || s2 == undefined) return -1;
-	   if (s1.length != s2.length) return -1;
+	   if (s1.length < 16 || s2.length < 16) return -1;
 	   
 	   var distance = 0;
 	   
-	   for(var i = 0; i < s1.length; i++)
+	   for(var i = 0; i < 16; i++)
 	      if (s1.charAt(i) != s2.charAt(i))
 	         distance++;
 	   return distance;
